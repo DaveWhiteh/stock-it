ce8b42cf17cf544293415f9e6205c143950ce5e7
DaveWhiteh <david_whiteh@msn.com>
2024-03-01 00:08:35 +0000

    Amend the debug to false in the app.py

--- app.py
+++ app.py
@@ -619,4 +619,4 @@ def items_count():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
